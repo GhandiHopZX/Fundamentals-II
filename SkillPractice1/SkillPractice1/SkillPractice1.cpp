b07dbc090d929ec8c44a1758d6d6d7ab9078f6c0
solomon.pattee <solomon.pattee@NEITD-222506>
2019-08-28 21:18:24 -0500

    j

---
+++ SkillPractice1/SkillPractice1/SkillPractice1.cpp
@@ -0,0 +1,47 @@
+// SkillPractice1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/******************************
+   Solomon Pattee
+   8/28/2019
+   SkillPractice1.cpp
+   Inventory Management for the Bins
+********************************/
+
+// Headers
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+// Global variables
+const int NUM_OF_BINS = 10;
+BIN Bins[NUM_OF_BINS];
+
+struct BIN
+{
+	string partName;
+	int num;
+};
+
+// Function declarations
+
+int getBin(BIN[]);
+
+
+int main()
+{
+
+    cout << "Hello World!\n";
+
+	system("PAUSE");
+
+	return 0;
+}
+
+string DisplayBin(BIN)
+{
+	int input;
+	return input - 1;
+}
