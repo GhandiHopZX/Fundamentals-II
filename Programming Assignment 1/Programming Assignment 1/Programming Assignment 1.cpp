f3e09a49010234aafb93d6e7d08217b15c5f5b21
Solomon Pattee <azukastyer@gmail.com>
2019-09-08 02:11:33 -0500

    .

---
+++ Programming Assignment 1/Programming Assignment 1/Programming Assignment 1.cpp
@@ -0,0 +1,37 @@
+/******************************
+   Solomon Pattee
+   9/14/2019
+   Programming_Assignment_1.cpp
+   This assignment will focus on 
+   pointers and main functions 
+   based on 
+********************************/
+
+// Headers
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+// Global variables
+
+// Function declarations
+
+int main()
+{
+	//	Your code goes here
+
+
+	//	Make sure we place the end message on a new line
+	cout << endl;
+
+	//	The following is system dependent.  It will only work on Windows
+	system("PAUSE");
+
+	/*
+	// A non-system dependent method is below
+	cout << "Press any key to continue";
+	cin.get();
+	*/
+	return 0;
+}
