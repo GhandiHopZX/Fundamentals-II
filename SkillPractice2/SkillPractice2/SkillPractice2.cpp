7bf3175640641201f44dba5178d80ed8a92f6635
Solomon Pattee <azukastyer@gmail.com>
2019-09-09 19:07:47 -0500

    .

--- SkillPractice2/SkillPractice2/SkillPractice2.cpp
+++ SkillPractice2/SkillPractice2/SkillPractice2.cpp
@@ -22,7 +22,7 @@ int main()
 	//	Make sure we place the end message on a new line
 	cout << endl;
 
-	//	The following is system dependent.  It will only work on Windows
+	//	The following is system dependent. It will only work on Windows
 	system("PAUSE");
 
 	/*
