e43e71df5af5ddfc2234a77d804aedbc9d08c605
Solomon Pattee <azukastyer@gmail.com>
2019-09-12 02:30:29 -0500

    .

--- SkillPractice2/SkillPractice2/SkillPractice2.cpp
+++ SkillPractice2/SkillPractice2/SkillPractice2.cpp
@@ -7,25 +7,31 @@
 
 // Headers
 #include <iostream>
-#include <cstdlib>
-#include <string>
-#include <memory>
-#include <cmath>
 #include <iomanip>
 
 using namespace std;
 
-// Global variables
-
 // Function declarations
-
+int getMedian(int*, int);
 int* arrayAllocator(int);
 void X();
 
 int main()
 {
-	//	Calling the main program
-	X();
+	// Constants for the sizes 
+	int odd[] = { 1, 2, 3, 4, 5, 6, 7 };
+
+	int even[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+	cout << "Odd set" << endl;
+	// getting the median of the odd
+	int theMedodd = getMedian(odd, 7);
+
+	cout << "\nEven set" << endl;
+	// and even
+	int theMedeven = getMedian(even, 8);
+
+	/*X();*/
 
 	//	Make sure we place the end message on a new line
 	cout << endl;
@@ -33,13 +39,14 @@ int main()
 	//	The following is system dependent. It will only work on Windows
 	system("PAUSE");
 
-	/* ass
+	/* 
 	// A non-system dependent method is below
 	cout << "Press any key to continue";
 	cin.get();
 	*/
 	return 0;
 }
+
 // arrayAllocator function, takes an int for size, dynamically allocates
 // an array of ints on the number passed in, returns a pointer to the 
 // array
@@ -51,11 +58,8 @@ void X()
 	int numElements;	// holdt he number of elements
 	int* pointer = nullptr;	 // a pointer to the array, set to 0
 	int i;
-	double median;
-	double median2;
-	double median3;
-
-	//
+	
+	// Enter in size of the array
 	cout << "\enter the size of the int array: ";
 	cin >> numElements;
 
@@ -67,16 +71,9 @@ void X()
 		* (pointer + i) = i;
 
 	// display the values
-	/*cout << "\n\nHere are the values in the array:\n";
+	cout << "\n\nHere are the values in the array:\n";
 	for (i = 0; i < numElements; i++)
-		cout << "Element " << i << " has the value " << *(pointer + i) << endl;*/
-
-	median = pointer[(i / 2)];
-	median2 = (pointer[(i / 2)] + 1);
-	median3 = (median + median) / 2;
-
-	cout << "Here is the first middle number " << median << " and the other second middle number " << median2 << endl;
-	cout << " Here is the Median " << median3 << endl;
+		cout << "Element " << i << " has the value " << *(pointer + i) << endl;
 
 	// deallocate the array
 	delete[] pointer;
@@ -84,6 +81,39 @@ void X()
 
 }
 
+
+int getMedian(int* array0, int size)
+{
+	// formatting
+
+	cout << setprecision(2) << endl;
+
+	// doubles for the output
+	int* pointer = nullptr;
+	int count = 0; // for the loop
+	double median = 0;
+	double median2 = 0;
+	double median3 = 0;
+
+	// allocation hold
+	pointer = arrayAllocator(size);
+
+	// counting 
+	// fill the array with values 
+	for (int i = 0; i < size; i++)
+	{ 
+		pointer[i];
+	}
+
+	//median equations
+	median = pointer[(count / 2)];
+	median2 = (pointer[(count / 2)] + 1);
+	median3 = (median + median2) / 2;
+
+	cout << " Here is the Median " << median3 << endl;
+
+	return 0;
+}
 
 int* arrayAllocator(int num)
 {
