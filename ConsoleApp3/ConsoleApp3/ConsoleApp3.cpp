993d0523e278aee3cd5b19c71e1ad87a8f584cbf
solomon.pattee <solomon.pattee@NEITD-222506>
2019-09-11 20:05:15 -0500

    .

--- ConsoleApp3/ConsoleApp3/ConsoleApp3.cpp
+++ ConsoleApp3/ConsoleApp3/ConsoleApp3.cpp
@@ -2,13 +2,14 @@
    Solomon Pattee
    9/11/2019
    ConsoleApp3.cpp
-   Description
+   Description Chapter 12
 ********************************/
 
 // Headers
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <fstream>
 using namespace std;
 
 // Global variables
@@ -18,7 +19,42 @@ using namespace std;
 int main()
 {
 	//	Your code goes here
+	string name;		// to hold a file name
+	string inputLine;	// to hold a line of input
+	int lines = 0;	
+	ifstream file;
 
+	// get the name of the file
+	cout << "Enter the file name: ";
+	getline(cin, name);
+
+
+	// open the file
+	file.open(name);
+
+	// test for errors
+	if (!file)
+	{
+		cout << "Error opening " << name << endl;
+		exit(EXIT_FAILURE);
+	}
+	// display the first ten lines from the file
+	while (!file.eof() && lines < 10) // end of file
+	{
+		// get line
+		getline(file, inputLine, '\n');
+		// increment the line counter
+		lines++;
+		// display the line 
+		cout << inputLine << endl;
+	}
+
+	// if the file had less than ten lines, indicate that
+	if (lines < 10)
+		cout << "\nThe entire file hase been displayed. \n";
+
+	// close the file
+	file.close();
 
 	//	Make sure we place the end message on a new line
 	cout << endl;
