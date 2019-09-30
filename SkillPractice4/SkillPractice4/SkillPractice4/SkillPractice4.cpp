df32e99f76c57fd0a07a9805aa74aa433e299594
Solomon Pattee <azukastyer@gmail.com>
2019-09-30 01:02:45 -0500

    .

--- SkillPractice4/SkillPractice4/SkillPractice4/SkillPractice4.cpp
+++ SkillPractice4/SkillPractice4/SkillPractice4/SkillPractice4.cpp
@@ -1,20 +1,56 @@
-// SkillPractice4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/******************************
+   Solomon Pattee
+   9/30/2019
+   SkillPractice4.cpp
+   Description
+*******************************/
 
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+// Global variables
+// Test scores
+class testS 
+{
+	private:
+
+	public:
+		testS ();
+		~testS ();
+	
+	protected:
+
+};
+
+testS ::testS ()
+{
+}
+
+testS ::~testS ()
+{
+}
+
+// Function declarations
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//	Your code goes here
+
+
+	//	Make sure we place the end message on a new line
+	cout << endl;
+
+	//	The following is system dependent.  It will only work on Windows
+	system("PAUSE");
+
+	/*
+	// A non-system dependent method is below
+	cout << "Press any key to continue";
+	cin.get();
+	*/
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
