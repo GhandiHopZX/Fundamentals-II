abdc266c4ab2a38b99de5dfcc0851d4d5dc44a4c
Solomon Pattee <azukastyer@gmail.com>
2019-11-06 23:07:07 -0600

    .

---
+++ SkillPractice7/SkillPractice7/SkillPractice7/SkillPractice7.cpp
@@ -0,0 +1,20 @@
+/*
+//	Name: Solomon Pattee
+//	Date: 
+//	Filename: SkillPractice7.cpp
+//	Description: Absolute value calculator
+*/
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
