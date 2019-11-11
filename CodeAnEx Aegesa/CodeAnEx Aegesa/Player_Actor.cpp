d98fbf94e05fa329b0d6e0616dc84844b46463b3
Solomon Pattee <azukastyer@gmail.com>
2019-11-11 17:45:03 -0600

    .

--- CodeAnEx Aegesa/CodeAnEx Aegesa/Player_Actor.cpp
+++ CodeAnEx Aegesa/CodeAnEx Aegesa/Player_Actor.cpp
@@ -4,6 +4,28 @@
 
 Player_Actor::Player_Actor()
 {
+	// base stats
+	hp = 10;
+	sp = 5;
+	ap = 5;
+	dp = 0;
+	fp = 0;
+
+	// battler stats
+	ATK = 5;
+	DEF = 5;
+	SPD = 5;
+
+	AGI = 5;
+	DEX = 5;
+	END = 5;
+	INT = 5;
+	SPR = 5;
+	STR = 5;
+	CON = 5;
+
+	// exp
+	EXP = 0;
 }
 
 // use this to kill the player and when they die GAME OVER
--- CodeAnEx Aegesa/CodeAnEx Aegesa/Player_Actor.h
+++ CodeAnEx Aegesa/CodeAnEx Aegesa/Player_Actor.h
@@ -2,27 +2,33 @@
 class Player_Actor
 {
 private:
+
 	// variables
 	// statbase
 	int hp; // health
 	int sp; // special
-	int fp; // fighter(aural)
-	int ap; // abiliy
-	int dp; // dynamic points(obtained after a Turn A clash)
-	//
+	int fp; // fighter (aural)
+	int ap; // abiliy (optained after every level) (you'll also obtain -ap- from special items, Bosses and TurnA clashes)
+	int dp; // dynamic points (obtained after a Turn A clash for leveling)
+
+	// battler statbase <- ap goes here
 	int ATK; // attack
 	int DEF; // defence (armor based) 
+	int SPD; // speed
+
+	// ability stats <- dp goes here
 	int AGI; // agility
 	int DEX; // dexterity
 	int INT; // intelligence
 	int SPR; // spirit
-	int SPD; // speed
-	int STR; // strength
+	int STR; // strength (also effects status)
 	int END; // endurance (bodily based / also effects status)
+	int CON; // constitution (mental, body, soul, based / also effects status)
+
+	int EXP; // experience to obtain the per level ap gain
 
 public:
 	// variables
-
 
 	Player_Actor();
 
