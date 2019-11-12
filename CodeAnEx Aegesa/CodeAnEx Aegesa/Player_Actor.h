7fef18b6fbb0db48f43300e38c0dfc8f5952657e
Solomon Pattee <azukastyer@gmail.com>
2019-11-11 23:53:53 -0600

    .

--- CodeAnEx Aegesa/CodeAnEx Aegesa/Player_Actor.h
+++ CodeAnEx Aegesa/CodeAnEx Aegesa/Player_Actor.h
@@ -4,6 +4,7 @@ class Player_Actor
 private:
 
 	// variables
+
 	// statbase
 	int hp; // health
 	int sp; // special
@@ -25,17 +26,181 @@ private:
 	int END; // endurance (bodily based / also effects status)
 	int CON; // constitution (mental, body, soul, based / also effects status)
 
+	// experience
 	int EXP; // experience to obtain the per level ap gain
 
 public:
 	// variables
-
+	
 	Player_Actor();
 
+	// getters
+#pragma region getters
 
+		//Hp Getter
+	int getHp()
+	{
+		return hp;
+	};
 
+	//Sp Getter
+	int getSp()
+	{
+		return sp;
+	};
 
+	//Fp Getter
+	int getFp()
+	{
+		return fp;
+	};
 
+	//Ap Getter
+	int getAp()
+	{
+		return ap;
+	};
+
+	//Dp Getter
+	int getDp()
+	{
+		return dp;
+	};
+
+	//atk Getter
+	int getATK()
+	{
+		return ATK;
+	};
+
+	//def Getter
+	int getDEF()
+	{
+		return DEF;
+	};
+
+	//spd Getter
+	int getSPD()
+	{
+		return SPD;
+	};
+
+	//agi Getter
+	int getAGI()
+	{
+		return AGI;
+	};
+
+	//dex Getter
+	int getDEX()
+	{
+		return DEX;
+	};
+
+	//int Getter
+	int getINT()
+	{
+		return INT;
+	};
+
+	//spr Getter
+	int getSPR()
+	{
+		return SPR;
+	};
+
+	//str Getter
+	int getSTR()
+	{
+		return STR;
+	};
+
+	//end Getter
+	int getEND()
+	{
+		return END;
+	};
+
+	//con Getter
+	int getCON()
+	{
+		return CON;
+	};
+
+	//exp Getter
+	int getExp()
+	{
+		return EXP;
+	};
+#pragma endregion
+	// setters
+#pragma region setters
+
+	void setHp(int hpin)
+	{
+		hp = hpin;
+	}
+
+	void setSp(int spin)
+	{
+		sp = spin;
+	}
+
+	void setFp(int fpin)
+	{
+		fp = fpin;
+	}
+
+	void setAp(int apin)
+	{
+		ap = apin;
+	}
+
+	void setDp(int dpin)
+	{
+		dp = dpin;
+	}
+
+	void setSTR(int stin)
+	{
+		STR = stin;
+	}
+
+	void setATK(int atin)
+	{
+		ATK = atin;
+	}
+
+	void setDEF(int dein)
+	{
+		DEF = dein;
+	}
+	
+	void setAGI(int agin)
+	{
+		AGI = agin;
+	}
+
+	void setSPD(int sdin)
+	{
+		SPD = sdin;
+	}
+
+	void setEND(int send)
+	{
+		END = send;
+	}
+
+	void setCON(int coin)
+	{
+		CON = coin;
+	}
+
+	void setEXP(int expin)
+	{
+		EXP = expin;
+	}
+#pragma endregion
 
 	//~Player_Actor();
 };
