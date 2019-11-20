683757783c17966f24e0aa9581d6a7f6a5e6d523
Solomon Pattee <azukastyer@gmail.com>
2019-11-20 17:34:22 -0600

    .

--- SkillPractice8/SkillPractice8/SkillPractice8/SkillPractice8.cpp
+++ SkillPractice8/SkillPractice8/SkillPractice8/SkillPractice8.cpp
@@ -1,3 +1,10 @@
+/*
+Name: Solomon Pattee
+Date: 11/20/2019
+Filename: SkillPractice8.cpp
+Description: This program does multiplication through recursion
+*/
+
 #include <iostream>
 #include <string>
 #include <cstdio>
@@ -5,37 +12,6 @@
 #include <iomanip>
 
 using namespace std;
-
-//template <class T>
-//T factorial(T n)
-//{
-//	if (n == 0)
-//		return 1; // back to here
-//	else
-//	{
-//		unsigned
-//			long long int fact = recursiveMulti(n - 1);
-//		return fact * n; // from here
-//	}
-//	//return n;
-//}
-
-//template <class T>
-//T recursiveAddition(T x, T y)
-//{
-//	int z; // equals x * y
-//	z = x + y;
-//
-//	if (z >= x)
-//	{
-//		x += y;
-//		return x;
-//	}
-//	else
-//	{
-//		return z = x + y;
-//	}
-//}
 
 template <class T>
 T product(T x, T y)
@@ -74,13 +50,3 @@ int main()
 	system("PAUSE");
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
