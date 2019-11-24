d518d9e5146bc07f2b1e42dfa76be7bf207146a3
Solomon Pattee <azukastyer@gmail.com>
2019-11-23 21:08:44 -0600

    ,m

---
+++ Programming Assignment 5/Programming Assignment 5/Programming Assignment 5/Programming Assignment 5.cpp
@@ -0,0 +1,33 @@
+// Programming Assignment 5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int SIZE = 50;
+
+string testStrings[SIZE] =
+{ "ABLE WAS I ERE I SAW ELBA",
+"FOUR SCORE AND SEVEN YEARS AGO",
+"NOW IS THE TIME FOR ALL GOOD MEN",
+"DESSERTS I STRESSED",
+"AKS NOT WHAT YOUR COUNTRY CAN DO FOR YOU",
+"KAYAK" };
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
