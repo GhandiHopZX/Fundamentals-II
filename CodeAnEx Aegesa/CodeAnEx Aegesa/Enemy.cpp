c1c6680c7d9c2de4458469c6eb351d2aac829ed9
Solomon Pattee <azukastyer@gmail.com>
2019-11-25 02:28:00 -0600

    ok

--- CodeAnEx Aegesa/CodeAnEx Aegesa/Enemy.cpp
+++ CodeAnEx Aegesa/CodeAnEx Aegesa/Enemy.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
 #include <cstdio>
+#include <string>
+#include <map>
+#include <stack>
 #include "Enemy.h"
 
+using namespace std;
+
+Enemy::Enemy()
+{
+}
+
+Enemy::Enemy(elementType, int expA, int gA, int item)
+{
+}
--- CodeAnEx Aegesa/CodeAnEx Aegesa/Enemy.h
+++ CodeAnEx Aegesa/CodeAnEx Aegesa/Enemy.h
@@ -4,16 +4,32 @@
 #include <cstdio>
 #include "Player_Actor.h"
 #include "battleSystem.h"
+#include "inventory.h"
+#include "inventory.cpp"
 
-class Enemy : Player_Actor
+using namespace std;
+
+class Enemy : public Player_Actor
 {
 private:
-		
+
+	// check this for a weakness 
+	enum class elementType 
+	{
+		Water = 0, Fire = 1, Earth = 2, Air = 3, Lightning = 4, Phase = 5, Bio = 6, Light = 7, Dark = 8
+	}; 
+
+	int expA; //exp awarded
+
+	int goldA; //gold awarded
+
 public:
 	// constructor
 	Enemy();
 
-	// added enemy types and methods
+	Enemy(elementType, int expA, int gA, int item);
 
+	// added enemy types and methods
+	list <Enemy> enemyList = {};
 };
 
