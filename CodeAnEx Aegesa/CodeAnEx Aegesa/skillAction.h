6efbd19b881b5b0b99e84c0c43db9dc2706b7c43
Solomon Pattee <azukastyer@gmail.com>
2019-12-05 21:56:33 -0600

    ok

--- CodeAnEx Aegesa/CodeAnEx Aegesa/skillAction.h
+++ CodeAnEx Aegesa/CodeAnEx Aegesa/skillAction.h
@@ -3,16 +3,34 @@
 #include "Player_Actor.h"
 #include "Enemy.h"
 
+using namespace std;
+
 class skillAction
 {
 private:
 
 	// variables
-#pragma region vars
+#pragma region other_vars
 
 #pragma endregion
 
 public:
+	struct skill
+	{
+		int num;
+		string name;
 
+	#pragma region statModifiers
+
+	#pragma endregion
+
+	};
+
+	skill allSkills[50] = 
+	{
+		
+	};
+
+	//state changes - wip
 };
 
